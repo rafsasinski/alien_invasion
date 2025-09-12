c0f7585c9dfb80604f44db9e3b28b0a772dfcc9d
rafsasinski <rafsasinski@gmail.com>
2025-09-12 18:45:19 +0100

    Add bullet update and draw mehtods

--- bullet.py
+++ bullet.py
@@ -19,3 +19,16 @@ class Bullet(Sprite):
 
         # Store the bullet's positoin as a decimal value.
         self.y = float(self.rect.y)
+
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position
+        self.rect.y = self.y
+
+
+    def draw_bullet(self):
+        """Draw the bullet on the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
