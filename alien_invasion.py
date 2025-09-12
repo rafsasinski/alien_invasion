2346626afc43a11c36011a1cf78318ae27332ac3
rafsasinski <rafsasinski@gmail.com>
2025-09-12 15:14:09 +0100

    Refactor: Add _check_events helper method to deal with keyboard&mouse events

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,12 +25,7 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for th egame."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                print(event)
-                print(event.type)
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
@@ -38,6 +33,16 @@ class AlienInvasion:
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            print(f"{event.type}\t{event}")
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
