adb95d98276e9c6e72ef546c7ba2198a67e630fb
rafsasinski <rafsasinski@gmail.com>
2025-09-12 19:23:39 +0100

    Refactor: Move the update bullets logic to separate method

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,15 +30,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Get rid of bullets that have disappeared.
-            # INVESTIGATE: This is maybe not the most efficient way?
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-                    print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
 
 
@@ -76,6 +68,17 @@ class AlienInvasion:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet position
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        # INVESTIGATE: This is maybe not the most efficient way?
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+                print(len(self.bullets))
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
