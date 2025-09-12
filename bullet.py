d50e2d44a5eaa7d1cc4f14a51168af095f3d53cb
rafsasinski <rafsasinski@gmail.com>
2025-09-12 18:34:44 +0100

    Add bullet object

---
+++ bullet.py
@@ -0,0 +1,21 @@
+import pygame
+ifrom pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+
+    def __init__(self, ai_game):
+        """Create a bullet sprite/object at the ship's current position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # Create a bullet rect at (0, 0) and then set correct position.
+        # Since the bullet object is not based on the image we must build rect object
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Store the bullet's positoin as a decimal value.
+        self.y = float(self.rect.y)
