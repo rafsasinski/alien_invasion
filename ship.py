dcc32a7d9837bec9f0910b4f9931be59b9fb1788
rafsasinski <rafsasinski@gmail.com>
2025-09-24 17:25:02 +0100

    Implement: Alien-to-Ship Collision check

--- ship.py
+++ ship.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Ship:
-    """A class t omanage the ship."""
+    """A class to manage the ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
