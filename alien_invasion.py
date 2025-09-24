8ebd862038ad603a31447f41aff8398153e290d7
rafsasinski <rafsasinski@gmail.com>
2025-09-24 17:11:41 +0100

    Refactor: Move Alien-Bullet collision to separate method

--- alien_invasion.py
+++ alien_invasion.py
@@ -113,8 +113,11 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
                 print(len(self.bullets))
 
-        # Check for any bullets that have hit aliens.
-        # If so, get rid of the bullet and the alien.
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any ullets and aliens that have collided
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if not self.aliens:
