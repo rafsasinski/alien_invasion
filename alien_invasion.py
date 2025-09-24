affea6c00fcb555d3e078e22e30a07b37181bc32
rafsasinski <rafsasinski@gmail.com>
2025-09-24 17:09:13 +0100

    Repopulate Alien fleet whenever user kills them all

--- alien_invasion.py
+++ alien_invasion.py
@@ -106,16 +106,22 @@ class AlienInvasion:
         # Update bullet position
         self.bullets.update()
 
-        # Check for any bullets that have hit aliens.
-        # If so, get rid of the bullet and the alien.
-        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
-
         # Get rid of bullets that have disappeared.
         # INVESTIGATE: This is maybe not the most efficient way?
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
                 print(len(self.bullets))
+
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and crate new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
 
     def _update_aliens(self):
         """
