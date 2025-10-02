04605111361ba5b8360cb7cb9496b46ee9ca16ed
rafsasinski <rafsasinski@gmail.com>
2025-10-02 12:28:12 +0100

    Set game to Inactive state once Ship counter goes down, fix few bugs

--- alien_invasion.py
+++ alien_invasion.py
@@ -40,9 +40,12 @@ class AlienInvasion:
         """Start the main loop for th egame."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
 
@@ -151,20 +154,22 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
 
-        # Decrement ships_left
-        self.stats.ships_left -= 1
-        print(f">> SHIPS LEFT: {self.stats.ships_left}")
+        if self.stats.ships_left > 0:
+            # Decrement ships_left
+            self.stats.ships_left -= 1
 
-        # Get rid of any remaining aliens and bullets.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pause
-        sleep(0.5)
+            # Pause
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
 
     def _check_fleet_edges(self):
@@ -183,8 +188,8 @@ class AlienInvasion:
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
 
-        scren_rect = self.screen.get_rect()
-        for alien in self.alines.sprites():
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
             if alien.rect.bottom >= screen_rect.bottom:
                 # Treat this the same as if the ship got hit.
                 self._ship_hit()
